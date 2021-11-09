4ba9b4af96685f5abffdde249aa9c2b3eae58405
kamkonphrom <kamkonphrom@gmail.com>
2021-11-09 21:53:54 +0700

    Add function update person

--- controllers/person.js
+++ controllers/person.js
@@ -1,29 +1,30 @@
 const Person = require("../models/Person");
 
-exports.create = async (req, res) => {
-  try {
-    const { name } = req.body;
-    res.json(await new Person({ name }).save());
-  } catch (err) {
-    console.log(err);
-    res.status(400).send("Create Person Failed");
-  }
+exports.create = async(req, res) => {
+    try {
+        const { name } = req.body;
+        res.json(await new Person({ name }).save());
+    } catch (err) {
+        console.log(err);
+        res.status(400).send("Create Person Failed");
+    }
 };
-exports.list = async (req, res) => {
-  res.json(await Person.find({}).sort({ createdAt: -1 }).exec());
+exports.list = async(req, res) => {
+    res.json(await Person.find({}).sort({ createdAt: -1 }).exec());
 };
-exports.read = async (req, res) => {
-  res.send("hello read person");
+exports.read = async(req, res) => {
+    const persons = await Person.findOne({ _id: req.params.id }).exec();
+    res.json(persons);
 };
-exports.update = async (req, res) => {
-  res.send("hello update person");
+exports.update = async(req, res) => {
+    res.send("hello update person");
 };
-exports.remove = async (req, res) => {
-  try {
-    const deleted = await Person.findOneAndDelete({ _id: req.params.id });
-    res.json(deleted);
-  } catch (err) {
-    console.log(err);
-    res.status(400).send("Remove Person Failed");
-  }
+exports.remove = async(req, res) => {
+    try {
+        const deleted = await Person.findOneAndDelete({ _id: req.params.id });
+        res.json(deleted);
+    } catch (err) {
+        console.log(err);
+        res.status(400).send("Remove Person Failed");
+    }
 };
